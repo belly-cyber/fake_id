9f21d538402afb9f07916d25d272f6f7ce3ab5a3
miguel ball <miguel.balli69@gmail.com>
2020-07-03 15:05:50 -0400

    first upload

---
+++ fake_id.py
@@ -0,0 +1,106 @@
+import re
+import random
+import string
+import requests
+import os
+import sqlite3
+
+
+def random_string(word):
+        termination=random.choice(range(1,len(word)))
+        rand_string=word[:termination]
+        return(rand_string)
+
+def pass_maker():
+    special_characters='!@#$%&'
+    chars=random.choices(string.ascii_letters,k=16)
+    num=random.choice([1,2,3,4])
+    [chars.append(x) for x in random.choices(string.digits,k=num)]
+    num=random.choice([1,2,3,4])
+    [chars.append(x) for x in random.choices(special_characters,k=num)]
+    random.shuffle(chars)
+    return(''.join(chars))
+
+def table_maker(db,table_name,value_list):
+    curs=db.cursor()
+    curs.execute("CREATE TABLE IF NOT EXISTS "+table_name+" (id integer PRIMARY KEY,"+table_name+" text)")
+    
+    for x in set(value_list):
+        #print(x)
+        y=list(set(value_list)).index(x)+1
+        project=(y,x)
+        curs.execute('INSERT INTO '+table_name+'(id, '+table_name+') VALUES(?,?)',project)
+        database.commit()
+
+if "fake_id.db" in os.listdir("dbs/"):
+    dic={}
+    database=sqlite3.connect("dbs/fake_id.db")
+    curs=database.cursor()
+    
+    for table in curs.execute('SELECT name from sqlite_master where type= "table"').fetchall():
+        curs.execute('SELECT * FROM {}'.format(table[0]))
+        rows = curs.fetchall()
+        dic.update({table[0].split('_')[0]:[row[1] for row in rows]})
+    database.close()
+    
+        
+else:
+    print('no stored file found, parsing ssa.gov and mongabay.com for names')
+
+        raw_first_names=requests.get('https://www.ssa.gov/oact/babynames/decades/names2010s.html','utf-8')
+    first_names=[x[5:-5] for x in re.findall(r'<td >.*?</td>',raw_first_names.text)]
+    
+    raw_last_names=requests.get('https://names.mongabay.com/most_common_surnames.htm')
+    last_names=[x[8:-6] for x in re.findall(r'<tr><td>[a-zA-Z]{2,20}</td><',raw_last_names.text)]    
+    
+    raw_domains=requests.get('https://github.com/mailcheck/mailcheck/wiki/List-of-Popular-Domains')
+    email_domain=[x[1:-1] for x in re.findall(r'>[a-zA-Z].*?\.[a-zA-Z\.].*?<',raw_domains.text)]    
+    
+    #creates db if it doesnt exist
+    database=sqlite3.connect("dbs/fake_id.db")
+    curs=database.cursor()
+   
+    
+    #makes the tables
+    curs.execute("CREATE TABLE IF NOT EXISTS first_names (id integer PRIMARY KEY,first_names text)")
+    curs.execute("CREATE TABLE IF NOT EXISTS last_names (id integer PRIMARY KEY,last_names text)")
+    curs.execute("CREATE TABLE IF NOT EXISTS email_domain (id integer PRIMARY KEY,email_domain text)")
+    #saves changes
+    database.commit()
+    
+
+    table_maker(database,'first_names',first_names)
+    table_maker(database,'last_names',last_names)
+    table_maker(database,'email_domain',email_domain)
+    database.close()
+
+class identity:
+    
+    def __init__(self):
+
+        self.first=random.choice(dic['first'])
+        
+        self.last=random.choice(dic['last'])
+        
+        self.email=random.choice(dic['email'])
+        
+        self.random_digits=str(random.random())[2:random.choice([6,8])]
+        
+        self.varibles=[random_string(x) for x in [self.first,self.last,self.random_digits]]
+        [self.varibles.append(x) for x in ['.','.','.'][:random.choice([0,1,2,])]]
+        random.shuffle(self.varibles)
+        #print(self.varibles)
+        while ('.' in self.varibles[0] or '.' in self.varibles[-1] or '..' in ''.join(self.varibles)) == True:
+            #print(''.join(self.varibles))
+            random.shuffle(self.varibles)
+            #print(self.varibles)
+        self.passwd=pass_maker()
+        
+        self.random_email=''.join(self.varibles)+'@'+self.email
+        
+        self.info='\nfirst name: '+self.first+"\nlast  name: "+self.last+'\n\temail: '+self.random_email+'\n\t\tpassword:'+self.passwd+'\n'
+        
+        r = requests.get("https://thispersondoesnotexist.com/image", headers={'User-Agent': 'My User Agent 1.0'}).content
+        with open('id_face.jpg','wb') as f:
+            f.write(r)
+
